d9ebb8a8dd635bdf6a4f9595ad0ddc3a53206d3f
Astrow <88676947+Shnukle@users.noreply.github.com>
2022-08-20 17:55:42 -0400

    Update downloadcalc.py

--- downloadcalc.py
+++ downloadcalc.py
@@ -6,13 +6,15 @@ if format == "gigabytes":
     size = input("what is the size of the file? (gigabytes) ")
     calculate = float(size)*1000/float(speed)
     time = int(calculate/60)
-    print("it will take roughly",time,"minutes for your download to finish")
+    hours = int(time/60)
+    print("it will take roughly",time,"minutes, or", hours, " hours for your download to finish")
 
 elif format == "megabytes":
     size = input("what is the size of the file? (megabytes) ")
     calculate = float(size)/float(speed)
     time = int(calculate/60)
-    print("it will take roughly",time,"minutes for your download to finish")
+    hours = int(time/60)
+    print("it will take roughly",time,"minutes, or", hours, "hours for your download to finish")
 
 #if incorrect format is entered
 else: print("invalid format run program again")
